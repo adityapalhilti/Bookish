ad225e8a67f3d2bfc1e8d6b4b38d7fa3482f0006
adityapalhilti <PALADI@HILTI.com>
2023-02-16 11:15:52 +0530

    404 solving netlify

---
+++ src/layouts/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export const ErrorPage = () =>{
+      
+
+
+    return(
+            //localStorage.getItem("token")!=null?
+              <div>
+                <h2>
+                    You are not authorised to view this page.
+                    Please Login to view.
+                </h2>
+              </div>
+    );
+
+}
+
